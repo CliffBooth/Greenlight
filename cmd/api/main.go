a42444e65fa14ed52058b178b0c2cf23c47b310f
Valery Vysotsky <feraljerboa@gmail.com>
2024-02-27 13:03:59 +0300

    Set up database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/gofor-little/env"
+	_ "github.com/lib/pq"
 )
 
-//p.
+//p. 104
 
 const version = "1.0.0"
 
 type config struct {
 	port int
 	env string
+	db struct {
+		dsn string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime string
+	}
 }
 
 type application struct {
@@ -29,10 +40,33 @@ func main() {
 	var conf config
 	flag.IntVar(&conf.port, "port", 4000, "Api server port")
 	flag.StringVar(&conf.env, "env", "development", "Environment (development|staging|pruduction)")
-	
+
+	if err := env.Load(".env"); err != nil {
+		// panic(err)
+	}
+
+	default_dsn, _ := env.MustGet("DB_DSN")
+	flag.StringVar(&conf.db.dsn, "db-dsn", default_dsn, "PostgreSQL DSN")
+	flag.IntVar(&conf.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&conf.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&conf.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max idle time (10s|30m)")
 	flag.Parse()
+
+	if len(conf.db.dsn) == 0 {
+		panic("database dsn was not provided neither in .env file nor as a -db-dsn flag")
+	}
+
 	fmt.Println("port:", conf.port)
 	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+
+	db, err := openDB(conf)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection established")
 
 	app := &application{
 		config: conf,
@@ -51,8 +85,35 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 	logger.Printf("starting %s server on %s", conf.env, server.Addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime) 
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 func getLogger() *log.Logger {
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module greenlight.vysotsky.com
 go 1.22.0
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require (
+	github.com/gofor-little/env v1.0.16
+	github.com/lib/pq v1.10.9 // indirect
+)
